811f69d3782a6d78d3ee8c40b3f71440cf34a752
F1ko228 <nik-shestakov-03@mail.ru>
2023-06-09 21:39:29 +0700

    new calculator commit

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,24 @@
 "use strict";
 
-function User(name) {
-    this.name = name;
-    if (!new.target) {
-        return new User(name);
+function Calculator() {
+    this.read = function() {
+        this.meaning_1 = +prompt('Введите первое значение', "");
+        this.meaning_2 = +prompt('Введите второе значение', "");
+    };
+
+    this.sum = function() {
+        return this.meaning_1 + this.meaning_2;
+    };
+
+    this.mul = function() {
+        return this.meaning_1 * this.meaning_2;
     };
 };
 
-let nikita = new User('Nikita');
 
-alert(nikita.name);
+let calculator = new Calculator();
+calculator.read();
 
-
+alert( `Сумма чисел ${calculator.meaning_1} и ${calculator.meaning_2} равна ` + calculator.sum() );
+alert( `Произведение чисел ${calculator.meaning_1} и ${calculator.meaning_2} равно ` + calculator.mul() );
 
