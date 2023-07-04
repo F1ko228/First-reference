59dfcd6a5b47a3a954123574a7ffae5d9870e5a2
F1ko228 <nik-shestakov-03@mail.ru>
2023-07-04 15:33:13 +0700

    delete - commit

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,27 @@
 "use strict";
 
-function sumInput(arr) {
-    let sum = 0;
-    while(true) {
-        let meaning = prompt("Введите элемент для массива","");
-        if(meaning == "" || meaning == null || !isFinite(meaning)) {
-            break;
-        }
-        arr.push(+meaning);
-    }
-
-    for(let item of arr) {
-        sum += item;
-    };
-    return sum;
+function camelize(str) {
+    let strInFn = str.split('');
+    alert(strInFn);
+    let indexValue;
+    while (indexValue != '-1') {
+        indexValue = strInFn.indexOf('-');
+        if(indexValue == '-1') break;
+        alert(indexValue);
+        strInFn.splice(indexValue, 2, strInFn[indexValue + 1].toUpperCase());
+        alert(strInFn);
+    }; 
+    // indexValue = strInFn.indexOf('-');
+    // alert(indexValue);
+    // strInFn.splice(indexValue, 2, strInFn[indexValue + 1].toUpperCase());
+    // alert(strInFn);
+    // if(strInFn.indexOf('-') >= 0) {
+    //     camelize(strInFn)   
+    // };
+    return strInFn.join('');
 };
 
-let array = [];
+alert(camelize("backgound-color"));
+alert(camelize("list-style-image"));
+alert(camelize("-webkit-transition"));
 
-alert(sumInput(array));
