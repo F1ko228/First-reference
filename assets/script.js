7f8aa6021ca0cd54f33c09b49135bd511f24b7ba
F1ko228 <nik-shestakov-03@mail.ru>
2023-07-10 19:38:25 +0700

    use map method

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,18 @@
 "use strict";
 
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
 
 let users = [ vasya, petya, masha ];
 
-let names = users.map( item => item.name )
+let usersMapped = users.map( function(item) {
+    let fullName;
+    item.fullName = item.name + " " + item.surname;
+    delete item.name;
+    delete item.surname;
+    return item;
+} )
 
-alert( names ); // Вася, Петя, Маша
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // Вася Пупкин
