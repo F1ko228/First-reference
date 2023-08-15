4c9c941639ef90dc6f499c0d077c1b8c4709676d
F1ko228 <nik-shestakov-03@mail.ru>
2023-08-15 20:20:54 +0700

    Average age in object

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,21 @@
 "use strict";
 
-function shuffle(array) {
-  for(let i = array.lenght - 1; i > 0; i--) {
-    let j = Math.floor(Math.random * (i + 1));
-    [array[i], array[j]] = [array[i], array[j]];
+function getAverageAge(array) {
+  let countAge = 0;
+  let countUsers = 0;
+  for(let i = 0; i < array.length; i++) {
+    if(true) countUsers++;
   }
+  array.map(user => countAge+= user.age);
+  let averageAge = countAge/countUsers;
+  if(countAge%countUsers !== 0) return averageAge.toFixed(1);
+  return averageAge;
 }
 
-let count = {
-  "123": 0,
-  '132': 0,
-  '213': 0,
-  '231': 0,
-  '321': 0,
-  '312': 0,
-};
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 29 };
 
-for(let i = 0; i < 1000; i++) {
-  let arr = [1, 2, 3];
-  shuffle(arr);
-  count[arr.join("")]++;
-}
+let arr = [ vasya, petya, masha ];
 
-for(let key in count) {
-  alert(`${key}: ${count[key]}`)
-};
-
+alert(getAverageAge(arr)); 
