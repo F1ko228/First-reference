bf8b6cbe748237a4951026deb4acd2f45619cd53
F1ko228 <nik-shestakov-03@mail.ru>
2023-08-25 18:22:30 +0700

    practice object date

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,15 @@
 "use strict";
 
-let user = { 
-  name: "John", 
-  years: 30, 
-};
+function getWeekDay(date) {
+  let day = date.getDay();
+  if(day == 0) return "ВС"
+  if(day == 1) return "ПН"
+  if(day == 2) return "ВТ"
+  if(day == 3) return "СР"
+  if(day == 4) return "ЧТ"
+  if(day == 5) return "ПТ"
+  if(day == 6) return "СБ"
+}
 
-let {name, years: age, isAdmin = "false"} = user;
-
-alert( name ); // John
-alert( age ); // 30
-alert( isAdmin ); // false
+let date = new Date(2012, 0, 3);  // 3 января 2012 года
+alert( getWeekDay(date) );        // нужно вывести "ВТ"
