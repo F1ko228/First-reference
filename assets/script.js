372ad5b1ad2226776957253d1f693714de1a0efa
F1ko228 <nik-shestakov-03@mail.ru>
2023-08-25 19:15:12 +0700

    too practice date

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,9 @@
 "use strict";
 
-function getWeekDay(date) {
-  let day = date.getDay();
-  if(day == 0) return "ВС"
-  if(day == 1) return "ПН"
-  if(day == 2) return "ВТ"
-  if(day == 3) return "СР"
-  if(day == 4) return "ЧТ"
-  if(day == 5) return "ПТ"
-  if(day == 6) return "СБ"
+function getLastDayOfMonth() {
+  let date = new Date()
+  return 3600 * date.getHours() + 60 * date.getMinutes() +  date.getSeconds();
 }
 
-let date = new Date(2012, 0, 3);  // 3 января 2012 года
-alert( getWeekDay(date) );        // нужно вывести "ВТ"
+
+alert( getLastDayOfMonth() )
