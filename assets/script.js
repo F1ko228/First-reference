76b5aa236999d3d26134f26a74bf7d8fbecc9661
F1ko228 <nik-shestakov-03@mail.ru>
2023-08-28 02:07:10 +0700

    practice JSON

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,27 @@
 "use strict";
 
-function getLastDayOfMonth() {
-  let date = new Date()
-  return 3600 * date.getHours() + 60 * date.getMinutes() +  date.getSeconds();
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Совещание",
+  occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+  place: room
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+alert(JSON.stringify(meetup, function replacer(key, value) {
+  return (value == meetup && key != "") ? undefined : value
+}));
+
+/* в результате должно быть:
+{
+  "title":"Совещание",
+  "occupiedBy":[{"name":"Иванов"},{"name":"Петров"}],
+  "place":{"number":23}
 }
-
-
-alert( getLastDayOfMonth() )
+*/
